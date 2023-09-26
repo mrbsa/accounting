157f7e8421685e02ef41760fde6ddd9ffc67f8e9
clarabarretto <clarabarrettoestudo@gmail.com>
2023-09-26 10:57:25 -0300

    adding svg image

--- projeto-contabilidade/src/App.js
+++ projeto-contabilidade/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import { useState } from 'react';
+import welcomeImage from './assets/welcome.svg'
 
 function App() {
     const [activeTab, setTab] = useState('WELCOME');
@@ -11,13 +12,15 @@ function App() {
         <>
             { activeTab === 'WELCOME' && (
                 <div className='container'>
-                    {/* <img src="./assets/welcomepage.svg" alt="bem-vinde"/> */}
+                    <img className='welcome-image' src={welcomeImage} alt="bem-vinde"/>
                     <button onClick={() => setTab('TABLE')} className='generate-button'>Gerar dados</button>
                 </div>
             )}
 
             { activeTab === 'TABLE' && (
-                <h1>EAI PAPAI</h1>
+                <div className='main-container'>
+                    <h1>EAI PAPAI</h1>
+                </div>
             )}
         </>
     );
