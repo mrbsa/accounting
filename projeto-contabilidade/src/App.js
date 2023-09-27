98b23fcd02d92f8eb81c71f99bf448cc9138696c
clarabarretto <clarabarrettoestudo@gmail.com>
2023-09-27 15:17:24 -0300

    adding new page with dash

--- projeto-contabilidade/src/App.js
+++ projeto-contabilidade/src/App.js
@@ -2,43 +2,78 @@ import './App.css';
 
 import data from './data.json';
 import { useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Tooltip, XAxis, YAxis, Legend, BarChart, Bar } from "recharts";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import welcomeImage from './assets/welcome.svg';
 
+function findTop5StatesWithHighestExpenses(data) {
+    const expensesByState = {};
+
+    data.forEach(item => {
+        const state = item.uf;
+        const expense = item.value;
+
+        if (expensesByState[state]) {
+            expensesByState[state] += expense;
+        } else {
+            expensesByState[state] = expense;
+        }
+    });
+
+    const sortedStates = Object.entries(expensesByState).sort((a, b) => b[1] - a[1]);
+
+    const top5States = sortedStates.slice(0, 30);
+
+    return top5States;
+}
+const top5States = findTop5StatesWithHighestExpenses(data);
+
+top5States.forEach(([state, expense]) => {
+    console.log(`${state}: R$ ${expense.toFixed(2)}`);
+});
 
 function App() {
     const [activeTab, setTab] = useState('WELCOME');
 
-    console.log(data[1]);
+    const dados1 = data.splice(0, 1000);
 
-    const teste = [];
-    const dados1 = data.splice(0, 100);
+    const estados = {};
+    dados1.forEach((item) => {
+        const uf = item.uf;
+        const valor = parseFloat(item.value);
+        if (estados[uf]) {
+            estados[uf] += valor;
+        } else {
+            estados[uf] = valor;
+        }
+    });
 
-    for (let i = 0; i < dados1.length; i++) {
-        const dados = dados1[i];
-        const objetoFormatado = {
-            name: dados.account,
-            uv: dados.value,
-            pv: dados.population,
-            amt: dados.year
-        };
-        teste.push(objetoFormatado);
-    }
+    const dadosFormatados = Object.keys(estados).map((uf) => ({
+        estado: uf,
+        valor: estados[uf] / 1e9,
+    }));
 
     return (
         <>
             {activeTab === 'WELCOME' && (
-                <div className='container-initial'>
-                    <img className='welcome-image' src={welcomeImage} alt='bem-vinde' />
-                    <button onClick={() => setTab('GENERAL')} id='generate-button' className='btn btn-light'>Gerar dados</button>
-                </div>
+                <>
+                    <div className='container-initial'>
+                    <div>
+                        <img className='welcome-image' src={welcomeImage} alt='bem-vinde' />
+                        <p className='description'>
+                        Base de dados relacional composta pelas informações de contabilidade financeira que abarca despesas por função, no escopo das unidades federativas do Brasil, entre 2018 e 2021. Os possíveis filtros de pesquisa por campo são: Ano, Instituição (Governo do Estado determinado), Código do IBGE do estado, Unidade Federativa, População (quantidade de habitantes na Unidade Federativa como apurado pelo IBGE no ano especificado), Coluna (tipo de despesa), Identificador da Conta, e Valor da conta (em reais).
+                        </p>
+                    </div>
+                        <button onClick={() => setTab('GENERAL')} id='generate-button' className='btn btn-light'>Gerar dados</button>
+                    </div>
+                </>
             )}
 
             {activeTab === 'GENERAL' && (
                 <div className='general-div'>
                     <div className='main-container-general'>
-                        <h1 className='main-title'> Análise dos dados (colocar outro nome dps) </h1>
+                        <h1 className='main-title'>Análise de dados</h1>
                         <button onClick={() => setTab('WELCOME')} id="go-back-button" className='btn btn-primary'>Início</button>
                         <button onClick={() => setTab('CHARTS')} id="generate-charts-button" className='btn btn-secondary'>Gerar gráficos</button>
                         <div className='navbar-main-div'>
@@ -119,29 +154,23 @@ function App() {
             {activeTab === 'CHARTS' && (
                 <div className='general-div'>
                     <div className='main-container-general'>
-                        <h1 className='main-title'>Análise dos dados (colocar outro nome dps)</h1>
+                        <h1 className='main-title'>Análise de dados</h1>
                         <button onClick={() => setTab('WELCOME')} id="go-back-button" className='btn btn-primary'>Início</button>
-                        <button onClick={() => setTab('CHARTS')} id="generate-charts-button" className='btn btn-secondary'>Gerar gráficos</button>
+                        <button onClick={() => setTab('GENERAL')} id="generate-charts-button" className='btn btn-secondary'>Gerar tabela</button>
                     </div>
-                    <div className='chart-div'>
-                        <LineChart
-                            width={1500}
-                            height={500}
-                            data={teste}
-                            margin={{
-                                top: 5,
-                                right: 30,
-                                left: 20,
-                                bottom: 10,
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Line type="monotone" dataKey="uv" stroke="red" strokeDasharray="3 4 5 2" />
-                        </LineChart>
+                    <div className='dos-graficos'>
+                        <div className='chart-div-top5'>
+                            <BarChart width={1000} height={500} data={top5States}>
+                                <XAxis dataKey="0" />
+                                <YAxis
+                                    tickFormatter={(value) => `R$ ${(value / 1e9)} B`}
+                                />
+                                <Tooltip />
+                                <Legend />
+                                <Bar dataKey="1" name="Despesas" fill='#80A4ED' />
+                            </BarChart>
+
+                        </div>
                     </div>
                 </div>
             )}
